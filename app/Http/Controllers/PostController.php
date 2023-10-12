d86755176e2fd0aa3267cc52b0af902da507e9fa
Hiraya8823 <hirayama82007@yahoo.co.jp>
2023-10-12 22:40:39 +0900

    一覧機能一部完了

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,7 +16,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        return view('posts.index');
+        $posts = Post::with('user')->latest()->paginate(9);
+        return view('posts.index', compact('posts'));
     }
 
     /**
